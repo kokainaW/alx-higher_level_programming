688247a2d1d5698016b4748996f0bc249b10356b
kokainaW <williamkhoko@yahoo.com>
2023-06-27 17:28:04 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""it defines a class Square."""
+
+
+class Square:
+    """it represents a square."""
+
+    def __init__(self, size):
+        """it initializes a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
