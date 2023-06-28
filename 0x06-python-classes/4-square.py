d7f04679ed079a4256b3e1de09f5b6955e1bb1f2
kokainaW <williamkhoko@yahoo.com>
2023-06-28 18:14:26 +0000

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
@@ -28,4 +29,3 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-
