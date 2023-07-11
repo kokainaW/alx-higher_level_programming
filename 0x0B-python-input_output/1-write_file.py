127e80e3605660e73f9b4195877ec4c6e2169a36
kokainaW <williamkhoko@yahoo.com>
2023-07-11 17:19:21 +0000

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+    write_file()
+"""
+
+
 def write_file(filename="", text=""):
-    with open(filename, 'w') as z:
-        return z.write(text)
+    """
+        write_file writes a string to a text file.
+        Args:
+            filename (str): name of file.
+            text (str): text to be written.
+        Returns: number of bytes written.
+    """
+    with open(filename, "w", encoding='utf-8') as a_file:
+        return a_file.write(text)
