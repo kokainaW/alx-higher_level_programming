d1b0f7c9c3bbb715c82e16dfb87fc13d283647ad
kokainaW <williamkhoko@yahoo.com>
2023-07-11 16:19:32 +0000

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Will create a Rectangle class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle
+    Private instances attributes:
+        - width
+        - height
+    Public method area()
+    Inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """Initializes an instance
+
+        Args:
+            - width: rectangle width
+            - height: rectangle height
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Returns a formatted string"""
+
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        """Computes the area of the rectangle instance
+        Overwrites the area() method from BaseGeometry
+        """
+
+        return self.__width * self.__height
