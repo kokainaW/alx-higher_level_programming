fd93b3ccccd227f8c18449fc1f265cf941e4107d
kokainaW <williamkhoko@yahoo.com>
2023-08-15 16:43:27 +0100

    Task 12-16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Will lists rows with name value
+-- Execute: cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table WHERE name!='' OR name IS NOT NULL ORDER BY score DESC;
