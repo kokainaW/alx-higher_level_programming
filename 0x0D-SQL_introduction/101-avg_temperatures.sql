0b7637060f815ecaffd80cefbf9df11ef3a20369
kokainaW <williamkhoko@yahoo.com>
2023-08-15 16:47:12 +0100

    Task 100-103

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- Will list avergae temperatures
+-- Execute: cat 101-avg_temperatures.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
