0763234488acc2a335bf8519f9fc50d2aa5c2ec8
kokainaW <williamkhoko@yahoo.com>
2023-08-15 21:49:42 +0100

    update

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Will insert values
 -- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
