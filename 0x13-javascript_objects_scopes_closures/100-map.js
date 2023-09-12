86206ebabc16574ee01749fcf4dbcaabfc99d52c
kokainaW <williamkhoko@yahoo.com>
2023-09-13 00:44:12 +0100

    updated 101

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-// imports an array
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-    return (e * index);
-});
-console.log(mappedList);
+// imports an array and computes new array
+const { list } = require('./100-data');
+
+console.log(list);
+console.log(list.map((element, idx) => element * idx));
