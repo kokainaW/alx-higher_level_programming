827e6bedb8cd9142a820f127ff0b4ec3b9c8a020
kokainaW <williamkhoko@yahoo.com>
2023-09-12 22:06:58 +0100

    Java Warm Up

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+/* Print string “It Would Print Javascript is amazing” */
+const myVar = 'Javascript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* 12-object.js */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
