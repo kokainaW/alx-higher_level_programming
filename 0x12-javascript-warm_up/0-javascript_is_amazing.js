5dccff97d3d2561c2fc1eaffcc8cf1164ad0e500
kokainaW <williamkhoko@yahoo.com>
2023-09-12 23:01:12 +0100

    Java Warm Up update

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-/* Print string “It Would Print Javascript is amazing” */
+// It print string “It Would Print Javascript is amazing”
+
 const myVar = 'Javascript is amazing';
 console.log(myVar);
