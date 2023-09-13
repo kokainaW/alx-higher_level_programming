c91b0a6bc31b252c91dbd9798063f659d6f32639
Osakpamukhoko William Ofiare <96081824+kokainaW@users.noreply.github.com>
2023-09-13 01:28:35 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const arg in args) {
-  console.log(args[arg]);
+// prints 3 lines: (like 1-multi_languages.js)
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(langs[i]);
 }
