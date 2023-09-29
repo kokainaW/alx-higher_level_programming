5f38fa1bfa79c451792831bfd4fcd4671d485304
kokainaW <williamkhoko@yahoo.com>
2023-09-29 01:44:42 +0100

    updated peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """6-peak script"""
-
-
 def divide(array, low, high):
     """divide and conquer"""
-
     mid = int((high + low)/2)
     if array[mid-1] <= array[mid] >= array[mid+1]:
         return array[mid]
@@ -12,11 +9,8 @@ def divide(array, low, high):
         return divide(array, low, mid-1)
     elif array[mid] < array[mid+1]:
         return divide(array, mid+1, high)
-
-
 def find_peak(list_of_integers):
     """Find peak of a list"""
-
     if not list_of_integers:
         return None
     return divide(list_of_integers, 0, len(list_of_integers)-1)
